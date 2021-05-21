968b71f4d3a8447b06b8dc5fe6cc2ed98dc2b0b2
Ivan <ivangonzaleztorre@gmail.com>
2021-05-21 12:35:09 +0200

    Init

---
+++ 2_RunExperiment1_GPU1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#####################################
+# Script to initialize the server
+#####################################
+
+# VARIABLES ####################
+NAME=${NAME:-"wav2vec2"}
+CONTAINER=${CONTAINER:-"wav2vec2_1"}
+export NV_GPU="1"
+
+################################
+################################
+
+# Ensure that the server is closed when the script exits
+function cleanup_server {
+    echo "Shutting down ${CONTAINER} container"
+    docker stop ${CONTAINER} > /dev/null 2>&1
+}
+
+if [ "$(docker inspect -f "{{.State.Running}}" ${CONTAINER})" = "true" ]; then
+    if [ "$1" == "norestart" ]; then
+       echo "${CONTAINER} is already running ..."
+       exit 0
+    fi
+    cleanup_server || true
+fi
+
+# Run the container
+set -x
+nvidia-docker run -it -d --rm --name ${CONTAINER} --runtime=nvidia --shm-size=4g --ulimit memlock=-1 --ulimit stack=67108864 -v /home:/home -v $PWD:/workspace/wav2vec2/ ${NAME}
+set +x
+
+# Execute
+nvidia-docker exec -it ${CONTAINER} bash Experiments/1_Experiment1.sh
