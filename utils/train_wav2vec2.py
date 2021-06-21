d357337f6eff6386ae3eb6afbf69bd59d17b6327
Ivan <ivangonzaleztorre@gmail.com>
2021-06-21 10:01:10 +0200

    Init

--- utils/train_wav2vec2.py
+++ utils/train_wav2vec2.py
@@ -286,8 +286,8 @@ def main():
     # df_train = df_train[df_train["duration"] > 100]
     # df_test = df_test[df_test["duration"] > 100]
 
-    df_train = df_train[~df_train["duration"].str.contains("kansas12")]
-    df_test = df_test[~df_test["duration"].str.contains("kansas12")]
+    df_train = df_train[~df_train["file_cut"].str.contains("kansas12")]
+    df_test = df_test[~df_test["file_cut"].str.contains("kansas12")]
 
 
     df_train = df_train.reset_index(drop=True)
