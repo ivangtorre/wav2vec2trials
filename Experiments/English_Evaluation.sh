b29fd06d112ba1f20a0c54157fe948e2772334ab
Ivan <ivangonzaleztorre@gmail.com>
2021-06-27 19:30:29 +0200

    Init

--- Experiments/English_Evaluation.sh
+++ Experiments/English_Evaluation.sh
@@ -3,11 +3,11 @@
 
 ##### PARAMETERS ##################################
 MODEL_PATH=${1:-${MODEL_PATH:-"results/english.-WITH-CONTROLS.-EPOCH-10.LR_TYPE-linear.-21-06-25.10.45.40"}}
-DATA_DIR=${2:-${DATA_DIR:-"/data/CORPORA/ACOUSTIC_CORPUS/APHASIA/english/2021_06_24/data/"}}
-TEST_MILD=${3:-${TEST_MILD:-"/data/CORPORA/ACOUSTIC_CORPUS/APHASIA/english/2021_06_24/df_test_mild.csv"}}
-TEST_MODERATE=${4:-${TEST_MODERATE:-"/data/CORPORA/ACOUSTIC_CORPUS/APHASIA/english/2021_06_24/df_test_moderate.csv"}}
-TEST_SEVERE=${5:-${TEST_SEVERE:-"/data/CORPORA/ACOUSTIC_CORPUS/APHASIA/english/2021_06_24/df_test_severe.csv"}}
-TEST_VSEVERE=${6:-${TEST_VSEVERE:-"/data/CORPORA/ACOUSTIC_CORPUS/APHASIA/english/2021_06_24/df_test_very_severe.csv"}}
+DATA_DIR=${2:-${DATA_DIR:-"/data/CORPORA/ACOUSTIC_CORPUS/APHASIA/english/2021_06_17/data/"}}
+TEST_MILD=${3:-${TEST_MILD:-"/data/CORPORA/ACOUSTIC_CORPUS/APHASIA/english/2021_06_17/df_test_mild.csv"}}
+TEST_MODERATE=${4:-${TEST_MODERATE:-"/data/CORPORA/ACOUSTIC_CORPUS/APHASIA/english/2021_06_17/df_test_moderate.csv"}}
+TEST_SEVERE=${5:-${TEST_SEVERE:-"/data/CORPORA/ACOUSTIC_CORPUS/APHASIA/english/2021_06_17/df_test_severe.csv"}}
+TEST_VSEVERE=${6:-${TEST_VSEVERE:-"/data/CORPORA/ACOUSTIC_CORPUS/APHASIA/english/2021_06_17/df_test_very_severe.csv"}}
 ######################################################
 
 
